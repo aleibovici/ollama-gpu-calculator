95ab7bc39e50cab3134bb39f1537f6c00bc2d2f9
Luke Hofstetter <ldhofstetter@gmail.com>
2026-02-09 20:31:39 -0800

    feat: add workstation RDNA4 gpus to OllamaGPUCalculator specifications

--- src/OllamaGPUCalculator.js
+++ src/OllamaGPUCalculator.js
@@ -50,11 +50,14 @@ const unsortedGpuSpecs = {
     'rx7800xt': { name: 'Radeon RX 7800 XT', vram: 16, generation: 'RDNA3', tflops: 37, tdp: 263 },  // Correct: 263W
     'rx7700xt': { name: 'Radeon RX 7700 XT', vram: 12, generation: 'RDNA3', tflops: 35, tdp: 245 },  // Correct: 245W
 	'rx9070xt': { name: 'Radeon RX 9070 XT', vram: 16, generation: 'RDNA4', tflops: 195, tdp: 304 },  // taken from AMD.com spec sheet, TFLOPS from peak half-precision (FP16 Matrix) performance
-	'rx9070': { name: 'Radeon RX 9070', vram: 16, generation: 'RDNA4', tflops: 145, tdp: 220 },  // taken from AMD.com spec sheet, TFLOPS from peak half-precision (FP16 Matrix) performance
-	'rx9060xt1': { name: 'Radeon RX 9060 XT', vram: 16, generation: 'RDNA4', tflops: 103, tdp: 160 },  // taken from AMD.com spec sheet, TFLOPS from peak half-precision (FP16 Matrix) performance
-	'rx9060xt2': { name: 'Radeon RX 9060 XT', vram: 8, generation: 'RDNA4', tflops: 103, tdp: 150 },  // taken from AMD.com spec sheet, TFLOPS from peak half-precision (FP16 Matrix) performance
-	'rx9060xt3': { name: 'Radeon RX 9060 XT LP', vram: 16, generation: 'RDNA4', tflops: 100, tdp: 140 },  // taken from AMD.com spec sheet, TFLOPS from peak half-precision (FP16 Matrix) performance
-	'rx9060': { name: 'Radeon RX 9060', vram: 8, generation: 'RDNA4', tflops: 86, tdp: 132 },  // taken from AMD.com spec sheet, TFLOPS from peak half-precision (FP16 Matrix) performance
+	'rx9070': { name: 'Radeon RX 9070', vram: 16, generation: 'RDNA4', tflops: 145, tdp: 220 },  
+	'rx9060xt1': { name: 'Radeon RX 9060 XT', vram: 16, generation: 'RDNA4', tflops: 103, tdp: 160 },  
+	'rx9060xt2': { name: 'Radeon RX 9060 XT', vram: 8, generation: 'RDNA4', tflops: 103, tdp: 150 },  
+	'rx9060xt3': { name: 'Radeon RX 9060 XT LP', vram: 16, generation: 'RDNA4', tflops: 100, tdp: 140 },  
+	'rx9060': { name: 'Radeon RX 9060', vram: 8, generation: 'RDNA4', tflops: 86, tdp: 132 },  
+	'radaipro': { name: 'Radeon AI Pro R9700', vram: 32, generation: 'RDNA4', tflops: 191, tdp: 300 },  
+	'radaipros': { name: 'Radeon AI Pro R9700S', vram: 32, generation: 'RDNA4', tflops: 191, tdp: 300 },  // I might not be looking carefully enough, but... this + the GPU above seem basically identical 
+	'radaiprod': { name: 'Radeon AI Pro R9700D', vram: 32, generation: 'RDNA4', tflops: 99, tdp: 150 },
 };
 
 const gpuSpecs = Object.fromEntries(
