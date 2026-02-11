98929be392eecc57eac2a0640354fbb4a6e8dbf2
Luke Hofstetter <ldhofstetter@gmail.com>
2026-02-10 19:01:56 -0800

    chore: finish migration to bun.js
    Replaces dummy test statement with testing backed by vitest instead of
    jest and adds scripts. Changed App.test.js to App.test.jsx, since .jsx
    is expected by Vite and it will not treat .js as React by default.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "/ollama-gpu-calculator/",
+	test: {
+		environment: "jsdom",
+		globals: true,
+		setupFiles: "./src/setupTests.js",
+		css: true,
+	},
 });
